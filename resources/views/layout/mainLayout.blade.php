530510e95530b8e9b7727856115209be82f2d45b
stefano472 <stefanopiotti472@gmail.com>
2022-05-18 17:25:43 +0200

    basic layout

---
+++ resources/views/layout/mainLayout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="icon" href="favicon.ico">
+    <link rel="stylesheet" href="css/app.css">
+    {{-- <title>@yield('title')</title> --}}
+    <title>DC | DC Comics</title>
+</head>
+<body>
+    Hola
+    @yield('header')
+    @yield('main')
+    @yield('footer')
+</body>
+</html>
